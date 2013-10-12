f6a569c87f8705a54010045834089b8919f3b55a
PeeHaa <info@pieterhordijk.com>
2013-10-13 01:03:00 +0200

    Added project bootstrap

---
+++ bootstrap.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * This bootstraps the PHPoAuthProvider application
+ *
+ * PHP version 5.4
+ *
+ * @category   PHPoAuthProvider
+ * @author     Pieter Hordijk <info@pieterhordijk.com>
+ * @copyright  Copyright (c) 2013 Pieter Hordijk
+ * @license    http://www.opensource.org/licenses/mit-license.html  MIT License
+ * @version    0.0.1
+ */
+namespace PHPoAuthProvider;
+
+/**
+ * Bootstrap the PHPoAuthProvider library
+ */
+require_once __DIR__ . '/src/PHPoAuthProvider/bootstrap.php';
